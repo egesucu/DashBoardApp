74dbe688f560b91d0fed78af0deb7131b4b3da21
Ege Sucu <egesucu@icloud.com>
2021-10-19 19:23:46 +0300

    Initial Commit

---
+++ DashBoardApp/DashBoardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashBoardAppApp.swift
+//  DashBoardApp
+//
+//  Created by Ege Sucu on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DashBoardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
